684ef8fb6c3a400987e8ca3395c5ff501283935a
warlockja <warlockja@gmail.com>
2025-05-03 08:42:58 +0300

    switched to CF hibernation websockets

--- src/index.ts
+++ src/index.ts
@@ -28,21 +28,24 @@ export default {
 // Durable Object
 export class ChatRoom extends DurableObject {
 	clients: Set<WebSocket>;
+	storage: DurableObjectStorage;
 
-	constructor(ctx: DurableObjectState, env: Env) {
+	constructor(state: DurableObjectState, env: Env) {
 		// This is reset whenever the constructor runs because
 		// regular WebSockets do not survive Durable Object resets.
 		//
 		// WebSockets accepted via the Hibernation API can survive
 		// a certain type of eviction, but we will not cover that here.
-		super(ctx, env);
+		super(state, env);
 
+		// client sessions bound to this chat room instance
 		this.clients = new Set();
 		this.ctx.getWebSockets().forEach((websocket) => {
 			this.clients.add(websocket);
 		});
 
-		console.log('CLIENTS: ', this.clients, this.clients.size);
+		// previous messages storage
+		this.storage = state.storage;
 	}
 
 	async fetch(request: Request): Promise<Response> {
@@ -50,38 +53,43 @@ export class ChatRoom extends DurableObject {
 		const webSocketPair = new WebSocketPair();
 		const [client, server] = Object.values(webSocketPair);
 
-		// Calling `accept()` tells the runtime that this WebSocket is to begin terminating
+		// Calling `acceptWebSocket()` informs the runtime that this WebSocket is to begin terminating
 		// request within the Durable Object. It has the effect of "accepting" the connection,
 		// and allowing the WebSocket to send and receive messages.
-		server.accept();
+		// Unlike `ws.accept()`, `state.acceptWebSocket(ws)` informs the Workers Runtime that the WebSocket
+		// is "hibernatable", so the runtime does not need to pin this Durable Object to memory while
+		// the connection is open. During periods of inactivity, the Durable Object can be evicted
+		// from memory, but the WebSocket connection will remain open. If at some later point the
+		// WebSocket receives a message, the runtime will recreate the Durable Object
+		// (run the `constructor`) and deliver the message to the appropriate handler.
+		this.ctx.acceptWebSocket(server);
+
 		this.clients.add(server);
 
 		this.broadcastClientsCount();
-
-		// Upon receiving a message from the client, the server replies with the same message,
-		// and the total number of connections with the "[Durable Object]: " prefix
-		server.addEventListener('message', (event: MessageEvent) => {
-			this.clients.forEach((ws) => {
-				try {
-					ws.send(event.data);
-				} catch (error) {
-					this.clients.delete(ws);
-				}
-			});
-		});
-
-		// If the client closes the connection, the runtime will close the connection too.
-		server.addEventListener('close', (cls: CloseEvent) => {
-			server.close(cls.code, 'Durable Object is closing WebSocket');
-			this.clients.delete(server);
-
-			this.broadcastClientsCount();
-		});
 
 		return new Response(null, {
 			status: 101,
 			webSocket: client,
 		});
+	}
+
+	// hibernation websocket methods
+	async webSocketMessage(ws: WebSocket, message: ArrayBuffer | string) {
+		this.clients.forEach((ws) => {
+			try {
+				ws.send(message);
+			} catch (error) {
+				this.clients.delete(ws);
+				this.broadcastClientsCount();
+			}
+		});
+	}
+
+	async webSocketClose(ws: WebSocket, code: number, reason: string, wasClean: boolean) {
+		// If the client closes the connection, the runtime will invoke the webSocketClose() handler.
+		ws.close(code, 'Durable Object is closing WebSocket');
+		this.broadcastClientsCount();
 	}
 
 	broadcastClientsCount() {
